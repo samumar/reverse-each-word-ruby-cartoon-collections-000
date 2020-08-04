52395608646ffc4ffb24374167b28b11b30818f3
samumar43 <samumar43@gmail.com>
2020-08-04 16:13:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,9 @@ def reverse_each_word(sentence)
     word.reverse
   end).join(' ')
 end
+
+
+# line one starts the definition for the reverse_each_word method passing through the (sentence) argument.
+# line two sets the variable (reversed) to the do end block that sets the argument (sentence) to the split.collect method, splitting each element inside the (sentence) array, before collecting them. (word) represents the elements within the (sentence) array.
+# line three actually reverses each word seperately inside (sentence) using the .reverse method.
+# line four after the do end block, all words joins back together into one string using the .join('') method.
