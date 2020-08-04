fc672582af08516e0e3c248862521692855dfc95
samumar43 <samumar43@gmail.com>
2020-08-04 15:53:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(element)
   new_string = element.split("x")
-  reversed = (new_string.split.collect do |element|
+  new_string.collect do |element|
     element.reverse
-  end)join('')
+  end
 end
