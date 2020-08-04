6988d65bc100d86ddadf1c9a28b5dde4cd1d43ea
samumar43 <samumar43@gmail.com>
2020-08-04 15:51:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(element)
   new_string = element.split("x")
-  reversed = (new_string.split.collect do |element|)
+  reversed = (new_string.split.collect do |element|
     element.reverse
-  end
+  end)
 end
